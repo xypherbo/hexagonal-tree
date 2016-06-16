e53f59613c9b347dd3f04cd465d77bb8afca7424
Nuttapong <professor_bo@hotmail.com>
2016-06-16 18:43:51 +0700

    test mongooose

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/hexagonaltree');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("mongodb connected at "+'mongodb://localhost/hexagonaltree');
+});
+
+var movieSchema = new mongoose.Schema({
+  title: { type: String }
+, rating: String
+, releaseYear: Number
+, hasCreditCookie: Boolean
+});
+
+// Compile a 'Movie' model using the movieSchema as the structure.
+// Mongoose also creates a MongoDB collection called 'Movies' for these documents.
+var Movie = mongoose.model('Movie', movieSchema);
+
+var thor = new Movie({
+  title: 'Thor'
+, rating: 'PG-13'
+, releaseYear: '2011'  // Notice the use of a String rather than a Number - Mongoose will automatically convert this for us.
+, hasCreditCookie: true
+});
+
+thor.save(function(err, thor) {
+  if (err) return console.error(err);
+  console.dir(thor);
+});
+
+// Find a single movie by name.
+Movie.findOne({ title: 'Thor' }, function(err, thor) {
+  if (err) return console.error(err);
+  console.dir(thor);
+});
+
+// Find all movies.
+Movie.find(function(err, movies) {
+  if (err) return console.error(err);
+  console.dir(movies);
+});
+
+// Find all movies that have a credit cookie.
+Movie.find({ hasCreditCookie: true }, function(err, movies) {
+  if (err) return console.error(err);
+  console.dir(movies);
+});
+
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -26,14 +78,10 @@ app.get('/services/:servicename', function(request, response) {
 app.post('/process/:servicename/:action', function(request, response) {
 
     try {
-        
         var obj = require('./process/' + request.params.servicename + "/" + request.params.action);
         obj.doProcess(request, response);
-        
     } catch (error) {
-        
         response.json();
-        
     }
 
 })
